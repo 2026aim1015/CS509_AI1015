ac0949bc61d5ed52f009b43f11594f8306af5ffd
2026aim1015 <2026aim1015@iitrpr.ac.in>
2026-08-14 21:18:11 +0530

    add csr

---
+++ assignment_03/include/graph.h
@@ -0,0 +1,19 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+#include <vector>
+#include <string>
+
+struct CSRGraph
+{
+    int num_vertices = 0;
+    int num_edges = 0;
+    int source = 0;
+
+    std::vector<int> row_ptr;
+    std::vector<int> col_ind;
+    std::vector<double> weights;
+};
+
+CSRGraph loadCSRGraph(const std::string& filename);
+
+#endif
